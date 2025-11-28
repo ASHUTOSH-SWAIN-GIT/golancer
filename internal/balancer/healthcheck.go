ddd9f1c9b8029e8775a447575275526678fcf97a
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-11-29 03:37:58 +0530

    fix serverpool errors

--- internal/balancer/healthcheck.go
+++ internal/balancer/healthcheck.go
@@ -1,4 +1,5 @@
-package balancer 
+package balancer
+
 import (
 	"log"
 	"net/http"
@@ -7,47 +8,40 @@ import (
 
 const HealthCheckPath = "/health"
 
-
-func  CheckBackend(backend *Backend) {
+func CheckBackend(backend *Backend) {
 	healthURL := backend.URL.String() + HealthCheckPath
 
-	client := http.client{
-		Timeout : 5 * time.Second
+	client := http.Client{
+		Timeout: 5 * time.Second,
 	}
 
-	resp,err := client.Get(healthURL)
-	
+	resp, err := client.Get(healthURL)
+
 	isAlive := (err == nil && resp.StatusCode == http.StatusOK)
 
-	if isAlive() {
-		backend.SetAlive(isAlive)
+	backend.Setalive(isAlive)
 
-		if isAlive {
-			log.Printf("Backend %s is up" , backend.URL)
-		} else {
-			log.Printf("Backend %s is down , Error %v" , backend.URL err)
-		}
+	if isAlive {
+		log.Printf("Backend %s is up", backend.URL)
+	} else {
+		log.Printf("Backend %s is down , Error %v", backend.URL, err)
+	}
 
-		if resp != nil && resp.Body != nil {
-			resp.Body.Close()
-		}
+	if resp != nil && resp.Body != nil {
+		resp.Body.Close()
 	}
 }
 
-
-func StartHealthCheckLoop(s *ServerPool , interval time.Duration) {
+func StartHealthCheckLoop(s *ServerPool, interval time.Duration) {
 	go func() {
 		ticker := time.NewTicker(interval)
 
 		for range ticker.C {
 			backends := s.GetBackends()
 
-
-			for _,b := range backends {
+			for _, b := range backends {
 				go CheckBackend(b)
 			}
 		}
 	}()
 }
-
-
--- internal/balancer/serverpool.go
+++ internal/balancer/serverpool.go
@@ -1,56 +1,56 @@
 package balancer
-import(
-	"net.url"
+
+import (
+	"net/url"
 	"sync"
 	"sync/atomic"
 )
 
-type Backend struct{
-	URL *url.URL
-	mu sync.RWMutex
-	IsAlive bool
+type Backend struct {
+	URL               *url.URL
+	mu                sync.RWMutex
+	isAlive           bool
 	ActiveConnections int64
 }
 
-func(b *Backend) Setalive(alive bool){
+func (b *Backend) Setalive(alive bool) {
 	b.mu.Lock()
-	b.IsAlive()
+	b.isAlive = alive
 	b.mu.Unlock()
 }
 
-func(b *Backend)IsAlive(alive bool){
+func (b *Backend) IsAlive() bool {
 	b.mu.RLock()
-	alive = b.IsAlive
-	b.mu.RUnlock()
+	defer b.mu.RUnlock()
+	return b.isAlive
 }
 
-type ServerPool struct{
-	mu sync.RWMutex
+type ServerPool struct {
+	mu       sync.RWMutex
 	backends []*Backend
-	current uint64
+	current  uint64
 }
 
-func(s *ServerPool)AddBackend(backend *Backend){
+func (s *ServerPool) AddBackend(backend *Backend) {
 	s.mu.Lock()
-	s.backends = append(s.backends,backend)
+	s.backends = append(s.backends, backend)
 	s.mu.Unlock()
 }
 
-func(s *ServerPool)GetBackends *[]Backend{
+func (s *ServerPool) GetBackends() []*Backend {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	return s.backends
 }
 
-
-func(s *ServerPool)NextIndex() int{
-	return int(atomic.AddUint64(&s,current,1) % uint64(len(s.backends)))
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, 1) % uint64(len(s.backends)))
 }
 
-func(s *ServerPool)GetNextHealthyServer() *Backend{
+func (s *ServerPool) GetNextHealthyServer() *Backend {
 	s.mu.RLock()
 	defer s.mu.Unlock()
-	
+
 	TotalServers := len(s.backends)
 	if TotalServers == 0 {
 		return nil
@@ -58,17 +58,14 @@ func(s *ServerPool)GetNextHealthyServer() *Backend{
 
 	startIdx := s.NextIndex()
 
-	for i = 0 ; i < TotalServers ; i ++  {
+	for i := 0; i < TotalServers; i++ {
 		idx := (startIdx + i) % TotalServers
-		backend:= s.backends[idx]
-
-		if backend.IsAlive(){
-			}
-
+		backend := s.backends[idx]
+		if backend.IsAlive() {
+			atomic.StoreUint64(&s.current, uint64(idx))
 			return backend
 		}
-		return nil``
-
-
 	}
+
+	return nil
 }
